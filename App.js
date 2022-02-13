e3c67ae7b7d294b9fe52a9db76ea8c95c57ea348
adamsiwiec1 <73085927+adamsiwiec1@users.noreply.github.com>
2022-02-13 13:37:05 -0500

    update

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput } from 'react-native';
+import Person from './components/Person'
+
 
 export default function App() {
-  const [person, setPerson] = useState({name: 'log', age:99}) 
+  const [person, setPerson] = useState({}) 
 
   return (
+
+ // Update State Real Time as Typing with Text Input
     <View style={styles.container}>
       <Text>Enter name:</Text>
+
       <TextInput
-      style={styles.container}
+      multiline
+      style={styles.input}
       placeholder='e.g Raj Bal'
+      onChangeText={(val) => setPerson({name: val, age: person.age})}
       />
 
-      <Text>name: {person.name}. age: {person.age}</Text>
+
+      <Text>Enter age:</Text>
+      <TextInput
+      keyboardType='numeric' // Many different props you can add to a text input field
+      style={styles.input}
+      placeholder='e.g 20'
+      onChangeText={(val) => setPerson({name: person.name, age: val})}
+      />  
+
+      <Text>name: {person.name} age: {person.age}</Text>
     </View>
+
+
+
+
   );
 }
 
@@ -25,4 +45,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    borderWidth: 1,
+    borderColor: '#777',
+    padding: 8,
+    margin: 10,
+    width: 200,
+
+  }
 });
---
+++ components/Person.js
@@ -0,0 +1,11 @@
+import React, { useState, Component} from 'react';
+
+class Person extends React.Component {
+    constructor() {
+      // Super is used to honor the inheritence of the Component Class
+      super();
+      this.state = {}
+    }
+  }
+  
+  export default Person;
