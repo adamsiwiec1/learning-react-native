4df16d30d9adea37a305cd4a62dc0c872882abff
adamsiwiec1 <73085927+adamsiwiec1@users.noreply.github.com>
2022-02-13 12:53:22 -0500

    init

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+  const [person, setPerson] = useState({name: 'log', age:99}) 
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Enter name:</Text>
+      <TextInput
+      style={styles.container}
+      placeholder='e.g Raj Bal'
+      />
+
+      <Text>name: {person.name}. age: {person.age}</Text>
     </View>
   );
 }
